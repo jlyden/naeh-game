886f5d38813face83c6aae739bef441d8826897a
jjl <jlyden1@alumni.nd.edu>
2017-03-08 21:21:18 -0500

    Simplify models - remove relationships

--- flask/app/models.py
+++ flask/app/models.py
@@ -9,73 +9,20 @@ class Game(db.Model):
     round_over = db.Column(db.Boolean, default=True)
     diversion = db.Column(db.Boolean, default=False)
 
+    available = db.Column(db.Array(db.Integer))
+    market = db.Column(db.Array(db.Integer))
+    unsheltered = db.Column(db.Array(db.Integer))
+    intake = db.Column(db.Array(db.Integer))
+    emergency = db.Column(db.Array(db.Integer))
+    rapid = db.Column(db.Array(db.Integer))
+    outreach = db.Column(db.Array(db.Integer))
+    transitional = db.Column(db.Array(db.Integer))
+    permanent = db.Column(db.Array(db.Integer))
+
     score = db.relationship('Score')
-    available = db.relationship('Available')
-    market = db.relationship('Market')
-    unsheltered = db.relationship('Unsheltered')
-    intake = db.relationship('Intake')
-    emergency = db.relationship('Emergency')
-    rapid = db.relationship('Rapid')
-    outreach = db.relationship('Outreach')
-    transitional = db.relationship('Transitional')
-    permanent = db.relationship('Permanent')
 
     def __repr__(self):
         return "<%r's Game>" % (self.team_name)
-
-
-class Available(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
-    beads = db.Column(db.Array(db.Integer))
-
-
-class Market(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
-    beads = db.Column(db.Array(db.Integer))
-
-
-class Unsheltered(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
-    beads = db.Column(db.Array(db.Integer))
-
-
-class Intake(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
-    beads = db.Column(db.Array(db.Integer))
-
-
-class Emergency(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
-    beads = db.Column(db.Array(db.Integer))
-
-
-class Rapid(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
-    beads = db.Column(db.Array(db.Integer))
-
-
-class Outreach(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
-    beads = db.Column(db.Array(db.Integer))
-
-
-class Transitional(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
-    beads = db.Column(db.Array(db.Integer))
-
-
-class Permanent(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
-    beads = db.Column(db.Array(db.Integer))
 
 
 class Score(db.Model):
