b50235c760e1cd20f5024902dadf6540978da518
jjl <jlyden1@alumni.nd.edu>
2017-07-03 08:14:54 -0400

    Add record class

--- app/models/score.py
+++ app/models/score.py
@@ -2,21 +2,45 @@ import pickle
 from app import db
 
 
+class Record(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
+    round_count = db.Column(db.Integer, nullable=False)
+    board_name = db.Column(db.String, nullable=False)
+    beads_in = db.Column(db.Integer, default=0)
+    beads_out = db.Column(db.Integer, default=0)
+    end_count = db.Column(db.Integer, nullable=False, default=0)
+    note = db.Column(db.String)
+
+    def __repr__(self):
+        return "<Record: Game %r, Round %r, Board %r >" % (self.game_id,
+                                                           self.round_count,
+                                                           self.board_name)
+
+    def __init__(self, game_id, round_count, board_name):
+        self.game_id = game_id
+        self.round_count = round_count
+        self.board_name = board_name
+
+
 class Log(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
-    round_count = db.Column(db.Integer)
-    board_played = db.Column(db.Integer, default=0)
+    round_count = db.Column(db.Integer, nullable=False)
+    board_played = db.Column(db.String)
     moves = db.Column(db.PickleType)
 
     def __repr__(self):
-        return "<Game %r Log>" % (self.game_id)
+        return "<Log: Game %r, Round %r, Board %r >" % (self.game_id,
+                                                        self.round_count,
+                                                        self.board_played)
 
     def __init__(self, game_id, round_count, board_played, moves):
         self.game_id = game_id
         self.round_count = round_count
         self.board_played = board_played
         self.moves = pickle.dumps(moves)
+
 
 
 class Score(db.Model):
